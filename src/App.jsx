087536cdf30fe2b6069020be5fed48cdf2aeb565
sumairdeveloper <sumairbughio70@gmail.com>
2025-01-19 13:59:08 +0500

    ,,,

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/header/Header'
-import HeroSection from './components/hero-section/Hero-section'
+import HeroSection from './components/heroSection/heroSection'
 import './App.css'
 import AboutMe from './components/aboutme/about-me'
 import Services from './components/services/services'
